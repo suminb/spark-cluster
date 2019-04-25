4ca481f11c1a6edd0ac30ef830836b82721ea1e1
Sumin Byeon <suminb@gmail.com>
2019-04-26 01:45:42 +0900

    Use spark-master instead of localhost

--- env/spark-worker.sh
+++ env/spark-worker.sh
@@ -1,6 +1,6 @@
 #Environment variables used by the spark workers
 #Do not touch this unless you modify the compose master
-SPARK_MASTER=spark://localhost:7077
+SPARK_MASTER=spark://spark-master:7077
 #Allocation Parameters
 SPARK_WORKER_CORES=1
 SPARK_WORKER_MEMORY=1G
